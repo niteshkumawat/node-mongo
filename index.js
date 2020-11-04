5cb06318f681d6c31017816f7ddda87631b3df94
NiteshKumawat <nitesh.kumawat@carrier.com>
2020-11-04 17:33:28 +0530

    connect to mongoDB using mongoose

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 require('dotenv').config();
+const mongoose = require('mongoose');
+
 
 const app = express();
 
 app.get('/',(req, res)=>{
     res.send('hello world');
 })
+
+
+mongoose.connect(process.env.DB_CONNECTION, 
+    { useUnifiedTopology: true, useNewUrlParser: true }, ()=>{
+        console.log('connected to mongoose');
+    });
 
 app.listen(process.env.PORT,()=>{
     console.log('server is up and running');
